c4e0aef161cedb0ead5da3bd81787cee8b479eb7
arseny-chebyshev <arseny.chebyshev@gmail.com>
2022-12-09 21:56:41 +0600

    Изменение классов pull/push тасков

--- inviter/tasks.py
+++ inviter/tasks.py
@@ -15,9 +15,18 @@ from .models import User, Group, Invitation
 
 logger = get_task_logger(__name__)
 
-class PullTask:
+# Надристал ООП
+class BaseTelegramChatTask:
 
-    def __init__(self, chat: str, session: SessionStore): # Всё же лучше обрабатывать по одной, т.к. self.group
+    def _add_group_to_db(self, raw_group: dict, id_key: str, chat_url: str) -> Group:
+
+        db_group = Group.objects.filter(id=raw_group[id_key]).first()
+        if not self.db_group:
+            db_group = Group.objects.create(id=raw_group[id_key], link=chat_url, 
+                                            access_hash=raw_group['access_hash'])
+        return db_group
+
+    def __init__(self, chat: str, session: SessionStore):
 
         self.chat_url = chat
         self.session = session
@@ -25,15 +34,21 @@ class PullTask:
         self.client = get_client(session)
         self.client.connect()
         self.group = self.client.get_entity(self.chat_url)
+        self.raw_group = json.loads((self.group.to_json()))
+        self.chat_id_key = [key for key in self.raw_group.keys() 
+                            if key.endswith('id')][0] # id keys are different for Chats and Groups
 
-    def add_group_to_db(self):
-        raw_group = json.loads((self.group.to_json()))
-        id_key = [key for key in raw_group.keys() 
-                  if key.endswith('id')][0]  # id keys are different for Chats and Groups 
-        self.db_group = Group.objects.filter(id=raw_group[id_key]).first()
-        if not self.db_group:
-            self.db_group = Group.objects.create(id=raw_group[id_key], link=self.chat_url, 
-                                                 access_hash=raw_group['access_hash'])
+        self.db_group = self._add_group_to_db(self.raw_group, self.chat_id_key, self.chat_url)
+        
+    def _switch_bot(self):
+        self.client.disconnect()
+        self.session.pop('phone', None) # Если пользовательская сессия содержала данные для клиента - 
+                                        # они больше не валидны
+        self.session.save()
+        self.client = get_client(self.session)
+
+
+class PullTask(BaseTelegramChatTask):
     
     def add_users_to_db(self, user_infos: dict):
         self.db_users = list(User.objects.bulk_update_or_create([User(**user_info) 
@@ -57,9 +72,7 @@ class PullTask:
             self.session.save()                                        # session saving
         except (FloodWaitError, PeerFloodError):
             print(f"FloodWait error, switch bot")
-            self.client.disconnect()
-            self.session.pop('phone', None)                            # session update
-            self.session.save()                                        # session saving
+            self._switch_bot()
             self.pull_users()                                          # recursive pull
         except:
             traceback.print_exc()
@@ -71,75 +84,78 @@ class PullTask:
         self.client.disconnect()
 
 
-class PushTask:
+class PushTask(BaseTelegramChatTask):
 
-    def __init__(self, chat: str, session: SessionStore):
-        
-        self.chat_url = chat
-        self.session = session
+    class CouldNotJoinException(BaseException):
+        pass
+
+    def __init__(self, chat: str, session: SessionStore, 
+                 donor_groups: List(str), max_users: int, 
+                 max_users_per_group: int):
+
+        super().__init__(chat, session)
+
         self.users = []
+        self.max_users_per_group = max_users_per_group
+        self.max_users = max_users
+        
+        self.donor_groups = self._validate_donor_groups(donor_groups)
 
-        self.client = get_client(session)
-        self.client.connect()
-        self.group = self.client.get_entity(self.chat_url)
-        self.raw_group = json.loads((self.group.to_json()))
-        self.chat_id_key = [key for key in self.raw_group.keys() 
-                            if key.endswith('id')][0] # id keys are different for Chats and Groups 
+    def _validate_donor_groups(self, donor_groups: List(str)) -> List(Group):
 
-    def add_group_to_db(self):
-        self.db_group = Group.objects.filter(id=self.raw_group[self.chat_id_key]).first()
-        if not self.db_group:
-            self.db_group = Group.objects.create(id=self.raw_group[self.chat_id_key], link=self.chat_url, 
-                                                 access_hash=self.raw_group['access_hash'])
+        db_donor_groups = []
+        self.failed_donor_groups = []
 
-    def join_chat(self):
-        invitations = self.session.pop('invitations', {})
+        for donor_group in donor_groups:
+            db_group = Group.objects.filter(link=donor_group).first()
+            if not db_group:
+                self.failed_donor_groups.append(donor_groups.pop(donor_groups.index(donor_group)))
+            else:
+                group_batch = db_group.user.all().exclude(id__in=[inv.user.id for inv 
+                                                          in Invitation.objects.filter(group=self.db_group, 
+                                                                                       is_added=True)])
+                self.users += group_batch[:self.max_users_per_group]
+                db_donor_groups.append(db_group)
+
+        self.max_users_total = len(self.users) if len(self.users) < self.max_users_total else self.max_users_total
+
+        return db_donor_groups
+
+    def _join_chat(self):
         try:
             self.client(JoinChannelRequest(self.group))
         except (FloodWaitError, PeerFloodError):
             print("FloodWaitError, switch bot")
             self.client.disconnect()
-            self.session.pop('phone', None)
-            self.session.save()
+            self.session.pop('phone', None)                                     # session update
+            self.session.save()                                                 # session saving
             self.client = get_client(session_data=self.session)
-            self.join_chat()
+            self._join_chat()                                                   # recursive join
         except:
             traceback.print_exc()
             self.client.disconnect()
+            raise self.CouldNotJoinException
+
+    def push_users(self):
+
+        sub_batch_size = 7
+        index = int(self.session.get('index', 0))
+        invitations = self.session.get('invitations', {})
+
+        try:
+            self._join_chat()
+        except self.CouldNotJoinException:
             invitations[self.chat_url] = {'error': 'Could not join the group to add users to'}
             self.session['invitations'] = invitations
             self.session.save()
+            return None
 
-    def push_users(self, donor_groups: List(str), max_users_total: int, max_users_per_group: int):
-        
-        self.join_chat()
-
-        failed_pull_groups = []
-        sub_batch_size = 7
-        index = int(self.session.pop('index', 0))
-        invitations = self.session.pop('invitations', {})
-
-        for donor_group in donor_groups:
-            db_group = Group.objects.filter(link=donor_group).first()
-            if not db_group:
-                failed_pull_groups.append(donor_groups.pop(donor_groups.index(donor_group)))
-            else:
-                group_batch = db_group.user.all().exclude(id__in=[inv.user.id for inv 
-                                                             in Invitation.objects.filter(group=self.db_group, 
-                                                                                          is_added=True)])
-                max_users_per_group = len(group_batch) if len(group_batch) < max_users_per_group else max_users_per_group
-                self.users += group_batch[:max_users_per_group]
-
-        max_users_total = len(self.users) if len(self.users) < max_users_total else max_users_total
-
-        for donor_group in donor_groups:
-            db_group = Group.objects.filter(link=donor_group).first()
+        for donor_group in self.donor_groups:
             invitations[donor_group] = {"success": 0, "already_in": 0}
-            group_batch = db_group.user.all().exclude(id__in=[inv.user.id for inv 
+            group_batch = donor_group.user.all().exclude(id__in=[inv.user.id for inv 
                                                          in Invitation.objects.filter(group=self.db_group, 
                                                                                       is_added=True)])
-            max_users_per_group = len(group_batch) if len(group_batch) < max_users_per_group else max_users_per_group
-            for user in group_batch[:max_users_per_group]:
+            for user in group_batch[:self.max_users_per_group]:
                 try:
                     invited_before = Invitation.objects.filter(user=user, group=self.db_group).first()
                     if invited_before:
@@ -151,7 +167,7 @@ class PushTask:
                         invitation = Invitation.objects.update_or_create(user=user, 
                                                                          group=self.db_group, 
                                                                          is_added=True)[0]             
-                        invitations[donor_group]['success'] += 1     
+                        invitations[donor_group.link]['success'] += 1
                         index += 1
                         self.session['invitations'] = invitations
                         self.session.save()
@@ -170,16 +186,16 @@ class PushTask:
                     invitation = Invitation.objects.update_or_create(user=user, group=self.db_group, 
                                                                      error_message=f"{exc_tuple[0]}: {exc_tuple[1]}")[0]
                     try:      
-                        invitations[donor_group][invitation.error_message] += 1
+                        invitations[donor_group.link][invitation.error_message] += 1
                     except KeyError:
-                        invitations[donor_group][invitation.error_message] = 1
+                        invitations[donor_group.link][invitation.error_message] = 1
                     self.session['invitations'] = invitations
                     self.session.save()
                     continue
-                if index >= max_users_total:
+                if index >= self.max_users_total:
                     self.client.disconnect()
                     self.session['invitations'] = invitations
-                    self.session['failed_pull_groups'] = failed_pull_groups
+                    self.session['failed_pull_groups'] = self.failed_donor_groups
                     self.session.save()
                     break
                 if index % sub_batch_size == 0:
@@ -188,7 +204,7 @@ class PushTask:
                     time.sleep(seconds)    
         self.client.disconnect()
         self.session['invitations'] = invitations
-        self.session['failed_pull_groups'] = failed_pull_groups
+        self.session['failed_pull_groups'] = self.failed_donor_groups
         self.session.save()
 
 
@@ -196,8 +212,6 @@ class PushTask:
 @shared_task(bind=True)
 def pull_users(self, session_key, chats, use_celery_logger=False):
     session = SessionStore(session_key=session_key)
-    client = get_client(session)
-    client.connect()
     session['users'] = {}
     session['successful_groups'] = []
     for chat in chats:
@@ -209,125 +223,7 @@ def pull_users(self, session_key, chats, use_celery_logger=False):
 @shared_task(bind=True)
 def push_users(self, session_key, donor_groups, target_link, max_users_total, max_users_per_group):
     session = SessionStore(session_key=session_key)
-    print("Fetching client from push_users()..")
-    client = get_client(session)
-    print(f"Fetched client {client.session.filename} in task")
-    client.connect()
-
-    failed_pull_groups = []
-    sub_batch_size = 7
-    index = int(session.pop('index', 0))
-    invitations = session.pop('invitations', {})
-    total_users = []
-    # try to join target group
-    try:
-        entity = client.get_input_entity(target_link)
-        raw_group = json.loads((entity.to_json()))
-        id_key = [key for key in raw_group.keys() if key.endswith('id')][0]
-        target_group = Group.objects.filter(id=raw_group[id_key]).first()
-        if not target_group:
-            target_group = Group.objects.create(id=raw_group[id_key], link=target_link, 
-                                                access_hash=raw_group['access_hash'])
-        print(f"Joining chat {target_group}..")
-        client(JoinChannelRequest(entity))
-    except (FloodWaitError, PeerFloodError):
-        print("FloodWaitError, switch bot")
-        client.disconnect()
-        session.pop('phone', None)
-        session.save()
-        push_users(self, session_key, donor_groups, target_link, max_users_total, max_users_per_group)
-    except:
-        traceback.print_exc()
-        client.disconnect()
-        invitations[target_link] = {'error': 'Could not join the group to add users to'}
-        session['invitations'] = invitations
-        session['failed_pull_groups'] = failed_pull_groups
-        session.save()
-        return None
-    # clean donor groups (refactor to forms.py later)
-    for donor_group in donor_groups:
-        group_in_db = Group.objects.filter(link=donor_group).first()
-        if not group_in_db:
-            failed_pull_groups.append(donor_groups.pop(donor_groups.index(donor_group)))
-        else:
-            group_batch = group_in_db.user.all().exclude(id__in=[inv.user.id for inv 
-                                                         in Invitation.objects.filter(group=target_group, 
-                                                                                      is_added=True)])
-            max_users_per_group = len(group_batch) if len(group_batch) < max_users_per_group else max_users_per_group
-            total_users += group_batch[:max_users_per_group]
-            print(f"total_users: {len(total_users)}")
+    task = PushTask(chat=target_link, session=session, donor_groups=donor_groups, 
+                    max_users=max_users_total, max_users_per_group=max_users_per_group)
+    task.push_users()
     
-    # calculate the real total of users
-    max_users_total = len(total_users) if len(total_users) < max_users_total else max_users_total
-    # push users from donor groups
-    for donor_group in donor_groups:
-        invitations[donor_group] = {"success": 0, "already_in": 0}
-        group_batch = group_in_db.user.all().exclude(id__in=[inv.user.id for inv 
-                                                     in Invitation.objects.filter(group=target_group, 
-                                                                                  is_added=True)])
-        max_users_per_group = len(group_batch) if len(group_batch) < max_users_per_group else max_users_per_group
-        for user in group_batch[:max_users_per_group]:
-            try:
-                invited_before = Invitation.objects.filter(user=user, group=target_group).first()
-                if invited_before:
-                    print(f"User {user} has already been invited")
-                    if not invited_before.is_added:
-                        print(f"But was not added, adding again..")
-                        client(InviteToChannelRequest(raw_group[id_key], [user.id]))
-                        invitation = Invitation.objects.update_or_create(user=user, 
-                                                                         group=target_group, 
-                                                                         is_added=True)[0]             
-                        invitations[donor_group]['success'] += 1           
-                        index += 1
-                        session['invitations'] = invitations
-                        session.save()
-                    else:
-                        invitations[donor_group]['already_in'] += 1      
-                        session['invitations'] = invitations
-                        session.save()                
-                else:
-                    print(f"Adding user {user}..")
-                    client(InviteToChannelRequest(raw_group[id_key], [user.id]))
-                    time.sleep(0.25)
-                    invitation = Invitation.objects.update_or_create(user=user, 
-                                                                     group=target_group, 
-                                                                     is_added=True)[0]             
-                    invitations[donor_group]['success'] += 1     
-                    index += 1
-                    session['invitations'] = invitations
-                    session.save()
-            except (FloodWaitError, PeerFloodError):
-                print(f"FloodWaitError, switch_bot")
-                client.disconnect()
-                session.pop('phone', None)
-                session['index'] = index
-                session['invitations'] = invitations
-                client = get_client(dict(session))
-                session.save()
-                continue
-            except:
-                traceback.print_exc()
-                exc_tuple = sys.exc_info()
-                invitation = Invitation.objects.update_or_create(user=user, group=target_group, 
-                                                                 error_message=f"{exc_tuple[0]}: {exc_tuple[1]}")[0]
-                try:      
-                    invitations[donor_group][invitation.error_message] += 1
-                except KeyError:
-                    invitations[donor_group][invitation.error_message] = 1
-                session['invitations'] = invitations
-                session.save()
-                continue
-            if index >= max_users_total:
-                client.disconnect()
-                session['invitations'] = invitations
-                session['failed_pull_groups'] = failed_pull_groups
-                session.save()
-                break
-            if index % sub_batch_size == 0:
-                seconds = 30
-                print(f"Cooldown for {seconds} seconds")
-                time.sleep(seconds)    
-    client.disconnect()
-    session['invitations'] = invitations
-    session['failed_pull_groups'] = failed_pull_groups
-    session.save()
